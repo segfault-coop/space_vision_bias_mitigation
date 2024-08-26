0683b02130af01c8d100375e873599286ac78a4b
Sebastian Nae <naesebi2000@gmail.com>
2024-08-26 17:20:13 +0200

    Added the Hydra

---
+++ src/Hydra_MAE.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+from torchvision.transforms import RandomResizedCrop, RandomHorizontalFlip, Compose, ToTensor, ToPILImage
+import numpy as np
+from timm.models.vision_transformer import PatchEmbed,Block
+
+class SimpleMAE(nn.Module):
+    def __init__(self,img_size = 224, img_size_p = (32,32), embed_dim = 768, patch_size = (32,32), num_heads = 12, encoder_num_layers = 24, decoder_num_heads = 8, decoder_emb_size = 512, batch_idx = 1):
+        super().__init__()
+        self.batch_idx = batch_idx
+        self.patch_size = patch_size[0]
+        self.decoder_emb_size = decoder_emb_size
+        
+        self.pe = PatchEmbed(img_size=img_size_p, patch_size=patch_size, embed_dim=embed_dim, in_chans=3)
+        self.num_patches = (img_size // patch_size[0]) ** 2
+        self.pos_embed = nn.Parameter(torch.zeros(self.num_patches, embed_dim), requires_grad = False)
+        self.cls_token = nn.Parameter(torch.zeros(1,embed_dim))
+        self.encoder = Block(dim=embed_dim,num_heads=12)
+        self.decoder_embed = nn.Linear(embed_dim, decoder_emb_size, bias=True)
+        self.mask_token = nn.Parameter(torch.zeros(1,decoder_emb_size))
+        torch.nn.init.normal_(self.mask_token, std=.02)
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(self.num_patches, decoder_emb_size))
+        
+        self.decoder_block = Block(decoder_emb_size, decoder_num_heads, 4, qkv_bias=True, norm_layer=nn.LayerNorm)
+        
+        self.decoder_norm = nn.LayerNorm(decoder_emb_size)
+        self.decoder_pred = nn.Linear(decoder_emb_size, self.patch_size ** 2 * 3, bias = True)
+        
+        
+    def forward(self, img):
+        ## ENCODER
+        self.tokens = img.shape[0]
+        mask = torch.zeros(self.tokens).long()
+        if self.batch_idx == 1:
+            mask_idx = torch.arange(0,7)
+        elif self.batch_idx == 2:
+            mask_idx = torch.arange(7,14)
+        elif self.batch_idx == 3:
+            mask_idx = torch.arange(14,21)
+        else:
+            raise ValueError("Invalid batch_idx, 1,2,3 supported")
+        
+        mask[mask_idx] = 1
+        input_tokens = self.pe(img[~mask.bool(),...]).squeeze(1)
+        pos_input_tokens = input_tokens + self.pos_embed[~mask.bool(),...]
+        self.tokens = torch.cat([self.cls_token, pos_input_tokens])
+        encodings = self.encoder(self.tokens.unsqueeze(0)).squeeze(0)
+        
+        ## Decoder
+        decoder_tokens = self.decoder_embed(encodings)
+        tokens = torch.zeros((self.num_patches, self.decoder_emb_size))
+        tokens[~mask.bool()] = decoder_tokens[1:,:]
+        tokens[mask.bool()] = torch.cat([self.mask_token] * (self.num_patches - (~mask.bool()).sum()))
+        
+        tokens = tokens + self.decoder_pos_embed
+        tokens = torch.cat([decoder_tokens[:1,:], tokens])
+        
+        out_tokens = self.decoder_block(tokens.unsqueeze(0)).squeeze(0)
+        
+        out = self.decoder_norm(out_tokens.unsqueeze(0)).squeeze(0)
+        out = self.decoder_pred(out)
+        
+        final_pred = out[1:,:]
+        return final_pred, mask
+    
+    
+class HydraMAE(nn.Module):
+    def __init__(self, num_heads = 3, train_mode = False):
+        super().__init__()
+        self.num_heads = num_heads
+        self.mae_blocks = nn.ModuleList()
+        for i in range(self.num_heads):
+            head = SimpleMAE(batch_idx=(i+1))
+            self.mae_blocks.append(head)
+        
+        if train_mode == True:
+            for h in self.mae_blocks:
+                h.train()
+        
+        
+    def forward(self, x):
+        i1,i2,i3 = x[0],x[1],x[2]
+        o1,m1 = self.mae_blocks[0](i1)
+        o2,m2 = self.mae_blocks[1](i2)
+        o3,m3 = self.mae_blocks[2](i3)
+        outputs = torch.stack([o1,o2,o3])
+        masks = torch.stack([m1,m2,m3])
+        return outputs, masks
+        
+    @staticmethod
+    def hydra_loss(inputs, outputs, masks, pwr_weight):
+        inputs = [inp.flatten(1) for inp in inputs]
+        inputs = torch.stack(inputs)
+        mean = inputs.mean(dim=-1, keepdim=True)
+        var = inputs.var(dim=-1,keepdim=True)
+        inputs = ((inputs - mean) / (var + 1.e-6) ** .5) ** 3
+        token_lvl_loss = ((outputs - inputs) ** 2).mean(dim=-1)
+        loss_01 = (token_lvl_loss * masks).sum() / masks.sum()
+        return loss_01
+        
--- src/mae-temporal.py
+++ src/mae-temporal.py
@@ -1,51 +1,124 @@
-import math
 import numpy as np
 import torch
 import torch.nn as nn
 from PIL import Image
 from torchvision.transforms import RandomResizedCrop, RandomHorizontalFlip, Compose, ToTensor, ToPILImage
-import os
 import matplotlib.pyplot as plt
+import wandb
+import datasets
+import pandas as pd
+from tqdm import tqdm
+def wandb_init(config):
+    project_name = "temporal_mae"
+    wandb.init(
+        project=project_name,
+    )
 
 def create_patches(img: Image, patch_size: int) -> torch.Tensor:
     img = ToTensor()(img)
-    imgp = img.unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size).permute((0, 3, 4, 1, 2)).flatten(3).permute((3, 0, 1, 2))
+    imgp = img.unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size).permute((0, 3, 4, 1, 2)).flatten(
+        3).permute((3, 0, 1, 2))
     return imgp
 
-def show_patches(img:torch.Tensor) -> None:
+
+def show_patches(img: torch.Tensor) -> None:
     fix, ax = plt.subplots(figsize=(4, 4), nrows=8, ncols=8)
     for n, i in enumerate(img):
         ax.flat[n].imshow(ToPILImage()(i))
         ax.flat[n].axis("off")
     plt.show()
 
-patch_size = 32
 
-imgs = os.listdir('data/MOD_LSTD_E_downscaled')
+def mask_block(imgs, idx):
+    for i, img in enumerate(imgs):
+        mask = torch.ones_like(img)
+        mask[i::idx] = 0
+        imgs[i] = img * mask
+    return imgs
 
-# Get the first img
-img_file_batch = imgs[0:3]
-img_batch = [Image.open(f'data/MOD_LSTD_E_downscaled/{img}') for img in img_file_batch]
+def transforms(img_size:int):
+    return Compose([RandomResizedCrop(size=img_size, scale=[0.4, 1], ratio=[0.75, 1.33], interpolation=2), 
+                    RandomHorizontalFlip(p=0.5), 
+                    ToTensor()])
 
-patches_imgs = [create_patches(img, patch_size) for img in img_batch]
+def load_ds(imgs, patch_size, transforms):
+    img = transforms(imgs)
+    imgp = img.unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size).permute((0, 3, 4, 1, 2)).flatten(3).permute((3, 0, 1, 2))
+    return imgp
 
-idx = 8
+def create_tokens_and_mask(imgp, batch_idx):
+    tokens = imgp.shape[0]
+    mask = torch.zeros(tokens).long()
+    if batch_idx == 1:
+        mask_idx = torch.arange(0,7)
+    elif batch_idx == 2:
+        mask_idx = torch.arange(7,14)
+    elif batch_idx == 3:
+        mask_idx = torch.arange(14,21)
+    else:
+        raise ValueError("Invalid batch_idx, 1,2,3 supported")
+    
+    mask[mask_idx] = 1
+    print(mask)
+    return imgp, mask 
 
-mask = torch.ones_like(patches_imgs[0])
-mask[:idx] = 0
+def main():
+    data_imgs = datasets.load_dataset("spacevision-upb/MOD_LSTD_E_downscaled")
+    idx_df = pd.read_csv('local_ds.csv')['hf_idx']
+    imgs = data_imgs['train']['image']
+    imgs = [imgs[i] for i in idx_df]
+    imgs = [load_ds(img,patch_size = 32, transforms=transforms(224)) for img in imgs]
 
-mask_2 = torch.ones_like(patches_imgs[1])
-mask_2[idx:idx*2] = 0
-print(mask.shape)
+    from Hydra_MAE import SimpleMAE, HydraMAE
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-mask_3 = torch.ones_like(patches_imgs[2])
-mask_3[idx*2:idx*3] = 0
-
-masked_img1 = patches_imgs[0] * mask
-masked_img2 = patches_imgs[1] * mask_2
-masked_img3 = patches_imgs[2] * mask_3
-
-show_patches(masked_img1)
-show_patches(masked_img2)
-show_patches(masked_img3)
+    model = HydraMAE(train_mode=True)
+    print(model)
+    if not list(model.parameters()):
+        raise ValueError("The model has no parameters to optimize.")
+    # out, o_mask = model(test_imgs)
+    # print(out.shape)
+    # print(o_mask.shape)
+    # ll = HydraMAE.hydra_loss(test_imgs,out,o_mask,pwr_weight)
+    # print(ll)
+    pwr_weight = [1,1,1]
+    total_epochs = 10
+    loss_fn = HydraMAE.hydra_loss
+    optim = torch.optim.AdamW(model.parameters(), lr=1e-3,betas=(0.9, 0.999), weight_decay=0.05)
+    
+    optim.zero_grad()
+    avg_losses = []
+    for e in range(total_epochs):
+        model.train()
+        losses = []
+        progress_bar = tqdm(range(0, len(imgs), 3), desc=f"Epoch {e+1}/{total_epochs}", unit="batch")
+        
+        for i in progress_bar:
+            img_chunk = imgs[i:i+3]
+            if len(img_chunk) < 3:
+                raise Exception("Smth went v wrong")
+            # img_chunk = [img.to(device) for img in img_chunk]
+            out, o_mask = model(img_chunk)
+            ll = loss_fn(img_chunk, out, o_mask, pwr_weight)
+            # print(f"Loss {ll}")
+            losses.append(ll.item())
+            optim.zero_grad()
+            ll.backward()
+            optim.step()
+            progress_bar.set_postfix(loss=ll.item())
+        
+        avg_loss = np.mean(losses)
+        avg_losses.append(avg_loss)
+        print(f"Epoch {e+1}/{total_epochs}, Loss: {avg_loss}")
+    
+    torch.save(model.state_dict(), 'model.pth')
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, total_epochs + 1), avg_losses, marker='o', linestyle='-', color='b')
+    plt.xlabel('Epoch')
+    plt.ylabel('Average Loss')
+    plt.title('Average Loss Over Time')
+    plt.grid(True)
+    plt.show()
+if __name__ == "__main__":
+    main()
